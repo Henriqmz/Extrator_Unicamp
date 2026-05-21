7db418e184294124e8073073fd1aaa003f3f829c
Henriqmz <henriquemzaianz@gmail.com>
2026-05-21 17:58:02 -0300

    Add files via upload

--- código/saver.py
+++ código/saver.py
@@ -1,22 +1,37 @@
 import os
+
 import json
 
-def salvar_questoes(questoes, pasta="questoes_json"):
+def salvar_questoes(questoes, pasta):
+
     os.makedirs(pasta, exist_ok=True)
 
     for q in questoes:
-        nome = f"{pasta}/questao_{q.metadados.numero}.json"
+
+        edital = q.metadados.edital
+
+        ano = q.metadados.ano
+
+        tipo_ou_cor = q.metadados.tipo_ou_cor.replace("/", "-")
+
+        numero = q.metadados.numero
+
+        nome = os.path.join(pasta, f"{edital}_{ano}_{tipo_ou_cor}_{numero}.json")
 
         with open(nome, "w", encoding="utf-8") as f:
+
             json.dump(q.model_dump(), f, ensure_ascii=False, indent=2)
 
+def salvar_textos(textos, pasta, edital="unicamp", ano=2026, tipo_ou_cor="Q-X"):
 
-def salvar_textos(textos, pasta="textos_json"):
     os.makedirs(pasta, exist_ok=True)
 
-    for t in textos:
-        cod = "_".join(t.metadadosComp.codigos_questoes)
-        nome = f"{pasta}/texto_{cod}.json"
+    tipo_ou_cor_limpo = tipo_ou_cor.replace("/", "-")
+
+    for idx, t in enumerate(textos, 1):
+
+        nome = os.path.join(pasta, f"{edital}_{ano}_{tipo_ou_cor_limpo}_COMP_{idx}.json")
 
         with open(nome, "w", encoding="utf-8") as f:
+
             json.dump(t.model_dump(), f, ensure_ascii=False, indent=2)
